8f73739ef752b68a04a6bd4fb0396a413041a1a8
Nicolas Ambroa <ambroanicolas@gmail.com>
2023-02-02 20:06:46 -0300

    Changed Service to interface and implementation for ease of future mocking and responsibility division.

--- src/main/java/com/springnotes/services/NoteService.java
+++ src/main/java/com/springnotes/services/NoteService.java
@@ -1,42 +1,17 @@
 package com.springnotes.services;
 
-import com.springnotes.factory.NoteFactory;
 import com.springnotes.model.api.CreateNoteRequest;
 import com.springnotes.model.domain.Note;
-import com.springnotes.repository.NoteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.StreamSupport;
-
-@Component
-public class NoteService {
-
-    private final NoteRepository noteRepository;
-
-    private final NoteFactory noteFactory;
 
 
-    @Autowired
-    public NoteService(NoteRepository noteRepository, NoteFactory noteFactory) {
-        this.noteRepository = noteRepository;
-        this.noteFactory = noteFactory;
-    }
+public interface NoteService {
+    Note createNote(CreateNoteRequest createNoteRequest);
 
-    public Note createNote(CreateNoteRequest createNoteRequest) {
-        Note note = this.noteFactory.createFrom(createNoteRequest);
-        this.noteRepository.save(note);
-        return note;
-    }
+    List<Note> getAllNotes();
 
-    public List<Note> getAllNotes() {
-        Iterable<Note> notes = this.noteRepository.findAll();
-        return StreamSupport.stream(notes.spliterator(), false).toList();
-    }
-
-    public Optional<Note> getNote(String noteId) {
-        return this.noteRepository.findById(Long.valueOf(noteId));
-    }
+    Optional<Note> getNote(String noteId);
 }
