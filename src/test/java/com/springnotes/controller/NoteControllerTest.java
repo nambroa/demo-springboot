a16a35c119fe559b64cfe6bfb8281a79ce7e9811
Nicolas Ambroa <ambroanicolas@gmail.com>
2023-03-03 18:39:54 -0300

    Added test to test the getNote section of the service layer.
    Also Removed old junit4 engine.

--- src/test/java/com/springnotes/controller/NoteControllerTest.java
+++ src/test/java/com/springnotes/controller/NoteControllerTest.java
@@ -1,7 +1,6 @@
 package com.springnotes.controller;
 
 
-
 import com.springnotes.SpringNotesApplication;
 import com.springnotes.model.domain.Note;
 import com.springnotes.repository.NoteRepository;
@@ -20,6 +19,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 /*
 The integration tests need to start up a container to execute the test cases.
 Hence, some additional setup is required for this — all of this is easy in Spring Boot.
@@ -28,14 +30,18 @@ Hence, some additional setup is required for this — all of this is easy in Spr
 /*
 We can use the webEnvironment attribute of @SpringBootTest to configure our runtime environment.
 We're using WebEnvironment.MOCK here so that the container will operate in a mock servlet environment.
-
-
  */
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = SpringNotesApplication.class)
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:/application-integrationtest.properties")
 public class NoteControllerTest {
+
+    private static final String NOTE_TITLE = "NoteTitle";
+
+    private static final String NOTE_DESCRIPTION = "NoteDescription";
+
     @Autowired
     private MockMvc mvc;
 
@@ -45,15 +51,28 @@ public class NoteControllerTest {
     @Autowired
     private NoteService noteService;
 
-    @Before
-    public void setUp(){
-        Note note = new Note("Title", "Description");
 
+    private Note testNote;
+
+    @Before
+    public void setUp() {
+        Note note = new Note("NoteTitle", "NoteDescription");
         Mockito.when(noteRepository.findById(note.getId())).thenReturn(Optional.of(note));
 
+        testNote = note;
     }
+
     @Test
-    public void nothingTest(){
+    public void nothingTest() {
         assert true;
+    }
+
+    @Test
+    public void whenValidTitle_thenNoteShouldBeFound() {
+        Optional<Note> note = noteService.getNote(String.valueOf(testNote.getId()));
+
+        assert (note.isPresent());
+        assertThat(note.get().getTitle()).isEqualTo(NOTE_TITLE);
+        assertThat(note.get().getTitle()).isEqualTo(NOTE_DESCRIPTION);
     }
 }
